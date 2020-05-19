c159d1a6fb7a36df3609597a56b98823a04aae6a
itzamaraval <35281964+itzamaraval@users.noreply.github.com>
2020-05-18 23:53:50 -0500

    update conexion

--- mvc/models/conexion.php
+++ mvc/models/conexion.php
@@ -3,7 +3,7 @@
 	class Conexion{
 		public function conectar(){
 	
-			$link = new PDO("mysql:host=localhost;dbname=BaseDatos","root","root");
+			$link = new PDO("mysql:host=localhost;dbname=BaseDatos","rooadmint","a58f7bd26723b03e63663e9781d6e66088070d4b881fb6a1");
 			return $link;
 		}
 	}
