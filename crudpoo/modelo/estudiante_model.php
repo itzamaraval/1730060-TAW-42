b9ffb52a502a5a53c45743b24429ca2267b346e5
itzamaraval <35281964+itzamaraval@users.noreply.github.com>
2020-05-22 08:57:12 -0500

    Update estudiante_model.php

--- crudpoo/modelo/estudiante_model.php
+++ crudpoo/modelo/estudiante_model.php
@@ -20,10 +20,10 @@
         function create($data){
 
             $this->DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql="INSERT INTO estudiante(cedula,nombre,apellidos,promedio,edad,fecha)VALUES (?,?,?,?,?,?)";
+            $sql="INSERT INTO estudiante(cedula,nombre,apellidos,promedio,edad,fecha,universidad_id,facultad_id)VALUES (?,?,?,?,?,?,?,?)";
 
             $query = $this->DB->prepare($sql);
-            $query->execute(array($data['cedula'],$data['nombre'],$data['apellidos'],$data['promedio'],$data['edad'],$data['fecha']));
+            $query->execute(array($data['cedula'],$data['nombre'],$data['apellidos'],$data['promedio'],$data['edad'],$data['fecha'],$data['universidad_id'],$data['facultad_id']));
             Database::disconnect();       
 
         }
@@ -39,9 +39,9 @@
         //función para actualizar la información de un estudiante
         function update($data,$date){
             $this->DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "UPDATE estudiante  set  cedula=?, nombre =?, apellidos=?,promedio=?, edad=?, fecha=? WHERE id = ? ";
+            $sql = "UPDATE estudiante  set  cedula=?, nombre =?, apellidos=?,promedio=?, edad=?, fecha=?, universidad_id=?, facultad_id=? WHERE id = ? ";
             $q = $this->DB->prepare($sql);
-            $q->execute(array($data['cedula'],$data['nombre'],$data['apellidos'],$data['promedio'],$data['edad'],$data['fecha'], $date));
+            $q->execute(array($data['cedula'],$data['nombre'],$data['apellidos'],$data['promedio'],$data['edad'],$data['fecha'], $date, $data['universidad_id'],$data['facultad_id']));
             Database::disconnect();
 
         }
