bf78fb94e0874862a5fb1fcd257531266997e818
itzamaraval <1730060@upv.edu.mx>
2020-06-12 08:45:51 -0500

    update

--- 25-05-2020/views/template.php
+++ 25-05-2020/views/template.php
@@ -1,217 +1,114 @@
-
-
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Inventarios 2020</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="assets/plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="assets/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="assets/plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="assets/plugins/summernote/summernote-bs4.css">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<meta charset="utf-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+	<title>Inventarios | TAW | UPV</title> 
+	<!-- Tell the browser to be responsive to screen width --> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<!-- Font Awesome --> 
+	<link rel="stylesheet" href="views/assets/plugins/fontawesome-free/css/all.min.css"> 
+	<!-- Ionicons --> 
+	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> 
+	<!-- Tempusdominus Bbootstrap 4 --> 
+	<link rel="stylesheet" href="views/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"> 
+	<!-- iCheck --> 
+	<link rel="stylesheet" href="views/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+	<!-- JQVMap --> 
+	<link rel="stylesheet" href="views/assets/plugins/jqvmap/jqvmap.min.css"> 
+	<!-- Theme style --> 
+	<link rel="stylesheet" href="views/assets/dist/css/adminlte.min.css"> 
+	<!-- overlayScrollbars --> 
+	<link rel="stylesheet" href="views/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css"> 
+	<!-- Daterange picker --> 
+	<link rel="stylesheet" href="views/assets/plugins/daterangepicker/daterangepicker.css"> 
+	<!-- summernote --> 
+	<link rel="stylesheet" href="views/assets/plugins/summernote/summernote-bs4.css"> 
+	<!-- Google Font: Source Sans Pro --> 
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> 
+	<!-- DataTables --> 
+	<link rel="stylesheet" href="views/assets/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
 </head>
+
 <body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
+	<?php 
+		/* Se inicia la sesión y se verifica que se haya iniciado sesión correctamente para mostrar el tablero y el menu principal */
+		session_start();
 
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-    &nbsp;
-    </ul>
+		if (isset($_SESSION['validar']) && $_SESSION['validar'] == "true") {
+			include 'modules/navegacion.php';
+		}
+	?>
+	<div class="wrapper">
+		<div class="content-wrapper">
+			<section class="content">
+				<div class="container-fluid">
+					<div class="row mr-3 mt-2 mb-2">
+						<?php 
+							/* Se verifica que la vista actual sea el tablero, en ese caso se muestra el titulo de la vista */
+							if ($_GET['action'] == 'tablero') {
+						?>
+						<div class="col-sm-12">
+							<h1><b>Tablero</b></h1>
+						</div>
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      &nbsp;
-    </ul>
-  </nav>
-  <!-- /.navbar -->
+						<!-- Aquí va el contenido de la página -->
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index.php" class="brand-link">
-      <img src="assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">Inventarios 2020</span>
-    </a>
+						<?php 
+							}
+							$mvc = new MvcController();
+							$mvc->enlacesPaginasController();
+						?>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      
+	<?php 
+		/* Se verifica que la vista actual no sea login, en caso de que sea login, el footer del template no se muestra */
+		if ($_GET['action'] != 'ingresar') {
+	?>
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <li class="nav-header">MENÚ</li>
-          <li class="nav-item">
-            <a href="index.php" class="nav-link">
-                <i class="nav-icon fa fa-signal"></i>
-                <p>
-                Tablero
-                </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="index.php?action=registro" class="nav-link">
-                <i class="nav-icon fas fa-user-plus"></i>
-                <p>
-                Registro
-                </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="index.php?action=ingresar" class="nav-link">
-                <i class="nav-icon fas fa-sign-in-alt"></i>
-                <p>
-                Ingresar
-                </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="index.php?action=usuarios" class="nav-link">
-                <i class="nav-icon fas fa-users"></i>
-                <p>
-                Usuarios
-                </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="index.php?action=productos" class="nav-link">
-                <i class="nav-icon fas fa-boxes"></i>
-                <p>
-                Productos
-                </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="index.php?action=categorias" class="nav-link">
-                <i class="nav-icon fas fa-tags"></i>
-                <p>
-                Categorias
-                </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="index.php?action=salir" class="nav-link">
-                <i class="nav-icon fas fa-sign-out-alt"></i>
-                <p>
-                Salir
-                </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+	<?php 
+		}
+	?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark"></h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        
-        <!-- /.row -->
-        <!-- Main row -->
-        <!--<div class="row">-->
-          
-          <?php
-                $mvc = new MvcController();
-                $mvc -> enlacesPaginasController();
-          ?>
-        <!--</div>-->
-        <!-- /.row (main row) -->
-
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.4
-    </div>
-  </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="assets/plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="assets/plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<script src="assets/plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="assets/plugins/sparklines/sparkline.js"></script>
-<!-- JQVMap -->
-<script src="assets/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="assets/plugins/jquery-knob/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="assets/plugins/moment/moment.min.js"></script>
-<script src="assets/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Summernote -->
-<script src="assets/plugins/summernote/summernote-bs4.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="assets/js/adminlte.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="assets/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="assets/js/demo.js"></script>
-</body>
-</html>
+	<!-- jQuery --> 
+	<script src="views/assets/plugins/jquery/jquery.min.js"></script> 
+	<!-- jQuery UI 1.11.4 --> 
+	<script src="views/assets/plugins/jquery-ui/jquery-ui.min.js"></script> 
+	<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip --> 
+	<script> $.widget.bridge('uibutton', $.ui.button) </script> 
+	<!-- Bootstrap 4 --> 
+	<script src="views/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script> 
+	<!-- ChartJS --> 
+	<script src="views/assets/plugins/chart.js/Chart.min.js"></script> 
+	<!-- Sparkline --> <script src="views/assets/plugins/sparklines/sparkline.js"></script> 
+	<!-- JQVMap --> 
+	<script src="views/assets/plugins/jqvmap/jquery.vmap.min.js"></script> 
+	<script src="views/assets/plugins/jqvmap/maps/jquery.vmap.world.js"></script>
+	<!-- jQuery Knob Chart --> 
+	<script src="views/assets/plugins/jquery-knob/jquery.knob.min.js"></script> 
+	<!-- daterangepicker --> 
+	<script src="views/assets/plugins/moment/moment.min.js"></script> 
+	<script src="views/assets/plugins/daterangepicker/daterangepicker.js"></script> 
+	<!-- Tempusdominus Bootstrap 4 --> 
+	<script src="views/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script> 
+	<!-- Summernote --> 
+	<script src="views/assets/plugins/summernote/summernote-bs4.min.js"></script> 
+	<!-- overlayScrollbars --> 
+	<script src="views/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> 
+	<!-- AdminLTE App --> 
+	<script src="views/assets/dist/js/adminlte.js"></script> 
+	<!-- DataTables --> <script src="views/assets/plugins/datatables/jquery.dataTables.js"></script> 
+	<script src="views/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+	<!-- page script --> 
+	<script> 
+		$(function () { 
+			$("#example1").DataTable(); 
+			$("#example2").DataTable(); 
+		}); 
+	</script> 
+</body> 
+</html> 
