08b5d03889e1cd446e1043f522e7540f89bc5c76
itzamaraval <1730060@upv.edu.mx>
2020-06-12 09:14:33 -0500

    update

--- 25-05-2020/views/modules/ingresar.php
+++ 25-05-2020/views/modules/ingresar.php
@@ -20,20 +20,21 @@
             <div class="card-body center">
 				<br>
 				<div class="input-group nb-3">
-					<input type="text" name="usuarioIngreso" id="txtUser" class="form-control" placeholder="Usuario" required>
+					<input type="text" name="txtUser" id="txtUser" class="form-control" placeholder="Username" required>
 					<div class="input-group-append">
 						<div class="input-group-text">
 							<span class="fas fa-user"></span>
 						</div>
 					</div>
-				</div>		     
-				<br>
-				<input type="password" name="passwordIngreso" id="txtPassword" class="form-control" placeholder="Password" required>
+				</div>
+				<div class="input-group nb-3">
+					<input type="password" name="txtPassword" id="txtPassword" class="form-control" placeholder="Password" required>
 					<div class="input-group-append">
 						<div class="input-group-text">
 							<span class="fas fa-user"></span>
 						</div>
 					</div>
+				</div>
             </div>
             
             <div class="card-footer">
