7630fd9028c07f2e0012ff1ee7e483efa48023c0
itzamaraval <1730060@upv.edu.mx>
2020-06-15 08:31:19 -0500

    update

--- 25-05-2020/views/template.php
+++ 25-05-2020/views/template.php
@@ -48,8 +48,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
-      <img src="assets/img/UP_Victoria.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: 1">
+      <img src="views/assets/img/UP_Victoria.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
       <span class="brand-text font-weight-light">Inventarios 2020</span>
     </a>
 
