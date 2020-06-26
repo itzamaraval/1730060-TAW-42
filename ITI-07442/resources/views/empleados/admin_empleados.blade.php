d9d6c77e96a7767948d33f3ab6fdcc0feaeb499a
itzamaraval <1730060@upv.edu.mx>
2020-06-26 08:51:22 -0500

    css y js

--- ITI-07442/resources/views/empleados/admin_empleados.blade.php
+++ ITI-07442/resources/views/empleados/admin_empleados.blade.php
@@ -1,13 +1,76 @@
 @extends ('layout.patron');
-@section ('titulo', 'Administración de empleados');
+@section ('titulo','Administración de empleados');
 @section ('contenido');
-    <!--Código HTML para mostrar el listado de empleados-->
+    <!-- Código HTML puro para mostrar el listado de empleados -->
     <div class="right_col" role="main">
         <div class="">
             <div class="page-title">
                 <div class="title_left">
-                    <h3> Administración de empleados </h3>
+                    <h3>Administración de empleados</h3>
                 </div>
             </div>
         </div>
-        
+
+        <div class="clearfix">
+            <div class="col-md-12 col-sm-12">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>Listado de empleados</h2>
+                        <div class="clearfix"></div>
+                        <div class="x_content">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="card-box table-responsive">
+                                        <table id="datatable-keytable" class="table table-striped table-bordered" style="width:100%;">
+                                            <thead>
+                                                <tr>
+                                                    <th></th>
+                                                    <th>Nombre(s)</th>
+                                                    <th>Apellido(s)</th>
+                                                    <th>Cédula</th>
+                                                    <th>Email</th>
+                                                    <th>Lugar de nacimiento</th>
+                                                    <th>Sexo</th>
+                                                    <th>Estado civil</th>
+                                                    <th>Teléfono</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach($empleados as empleados)
+                                                    <tr>
+                                                        _create_empleados_table
+                                                        <td>{{$empleado->nombres}}</td>
+                                                        <td>{{$empleado->apellidos}}</td>
+                                                        <td>{{$empleado->cedula}}</td>
+                                                        <td>{{$empleado->email}}</td>
+                                                        <td>{{$empleado->lugar_nacimiento}}</td>
+                                                        <td>{{$empleado->sexo}}</td>
+                                                        <td>{{$empleado->estado_civil}}</td>
+                                                        <td>{{$empleado->telefono}}</td>
+
+                                                        <!-- Agregar columna para editar y eliminar registro -->
+                                                        <td>
+                                                            <div style="display: flex">
+                                                                <a href="{{url('empleados/'.$empleado->id.'/edit')}}" class="btn btn-secondary">
+                                                                    <i class="fa fa-edit"></i>
+                                                                </a>
+                                                                <form action="{{url('empleados/'.$empleado->id)}}" method="POST">
+                                                                    {{csrf_field()}}
+                                                                    {{method_field('DELETE')}}
+                                                                    <button class='btn btn-danger'><i class='fas fa-trash'></i></button>
+                                                                </form>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
